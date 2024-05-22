439ee997fb3242b5e5c3107c457fe0e032b049c6
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2024-05-22 11:38:47 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -12,7 +12,6 @@ storageBucket : process.env.REACT_APP_STORAGE_BUCKET,
 messagingSenderId : process.env.REACT_APP_MESSAGING_SENDING_ID,
 appId : process.env.REACT_APP_APP_ID
 };
-console.log(firebaseConfig.apiKey);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);

9f84bb3eae877132911e712c933be127c7c3d972
Yashika Verma <96378034+yashikaverma8112@users.noreply.github.com>
2024-06-03 10:53:18 +0530

    Update LeftPanel.jsx

--- frontend/src/components/LeftPanel.jsx
+++ frontend/src/components/LeftPanel.jsx
@@ -5,7 +5,7 @@ export const LeftPanel = () => {
   return (
     <div className='widget' >
         <div className="widget__header">
-     
+     <h5 className='text-success fw-bold text-center'>Coming Soon !!</h5>
           <h5>Space to follow</h5>
         </div>
         <div className="widget__contents">
